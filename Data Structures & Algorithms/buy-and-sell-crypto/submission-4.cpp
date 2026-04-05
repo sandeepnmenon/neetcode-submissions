c4a037484d40b22f0e0a8416ef13c32a7c20b596
Sandeep Menon <menonsandu@gmail.com>
2026-04-05 16:53:43 -0700

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int result=0;
+        int min_buy=INT_MAX;
+        for(int i=0;i<n;++i) {
+            if(prices[i]<min_buy){
+                min_buy=prices[i];
+            }
+            else if(prices[i]-min_buy >= result){
+                result = prices[i]-min_buy;
+            }
+        }
+
+        return result;
+    }
+};
